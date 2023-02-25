7bdfd3c919eb50692558e0240d75d5d31624b5aa
Tony Chow <tchow9@gmail.com>
2023-02-24 18:57:39 -0800

    Working progress on ISS Spotter II

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const {fetchMyIP} = require('./iss');
+const {fetchMyIP, fetchCoordsByIP} = require('./iss');
 
 fetchMyIP((error, ip) => {
   if (error) {
@@ -7,3 +7,10 @@ fetchMyIP((error, ip) => {
   }
   console.log('It worked! Returned IP: ', ip);
 });
+
+fetchCoordsByIP((ip, error) => {
+  if (error) {
+    console.log(error)
+  }
+  console.log(ip)
+})
--- iss.js
+++ iss.js
@@ -9,7 +9,6 @@
 const request = require('request');
 const fetchMyIP = function(callback) {
   request('https://api.ipify.org?format=json', (error, response, body) => {
-    const data = JSON.parse(body);//converts the string into an object
     //error can be set if invalid domain, user is offline, etc.
     if (error) {
       callback(error, null);
@@ -21,10 +20,29 @@ const fetchMyIP = function(callback) {
       callback(Error(msg), null);//Error() creates a new Error objec that we can pass around. In this case we pass it back to the callback to indicate that something went wrong
       return;
     }
+    const data = JSON.parse(body);//converts the string into an object
     callback(null, data['ip']);//returns the IP through callback, since there is no erro, we fill the parameter with a null
     return;
   });
-  // use request to fetch IP address from JSON API
 };
 
-module.exports = { fetchMyIP };
+const fetchCoordsByIP = function(ip, callback) {
+  request(`http://ipwho.is/${ip}`, (error, response, body) => {
+    if (error) {
+      console.log(error, null)
+    }
+    const data = JSON.parse(body);
+    const latitude = data["latitude"]
+    const longitude = data['longitude']
+    console.log(latitude)
+    console.log(longitude)
+    result = { latitude, longitude }
+    console.log(result)
+  })
+}
+
+// console.log(fetchCoordsByIP('69.172.174.57', null))
+console.log(fetchCoordsByIP('42', null))
+
+
+module.exports = { fetchMyIP, fetchCoordsByIP };
